eeb838f010cf9b6ca851095afd7b13f0754dff83
toyosi001 <97127198+1toyosi@users.noreply.github.com>
2022-05-28 16:00:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let display = document.getElementById('display');
 
 let buttons = Array.from(document.getElementsByClassName('button'));
-// console.log(buttons);
 
 buttons.map(button => {
     button.addEventListener('click', (e) => {
@@ -22,7 +21,7 @@ buttons.map(button => {
                 }
                 break;
             default:
-                display.innerText += e.target.innerText;
+               display.innerText += e.target.innerText;
         }
     })
 })
